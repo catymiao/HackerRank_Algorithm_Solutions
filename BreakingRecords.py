dd16a5ff03969b7a18084144388e4b59f6f1aaa4
catymiao <33770231+catymiao@users.noreply.github.com>
2021-11-13 16:00:43 -0600

    Update BreakingRecords.py

--- BreakingRecords.py
+++ BreakingRecords.py
@@ -5,7 +5,7 @@ def breakingRecords(scores):
     break_low_ct = 0
     break_high_ct = 0
     for i in range(1,len(ls_scores),1):
-        if ls_scores[i] >max(ls_scores[:i]):
+        if ls_scores[i] > max(ls_scores[:i]):
              break_high_ct += 1
         if ls_scores[i] < min(ls_scores[:i]):
             break_low_ct += 1 
