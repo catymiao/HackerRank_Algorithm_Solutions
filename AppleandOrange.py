7ba02a73b2eeb38566e7d8cbda1f1f500ddfc511
catymiao <33770231+catymiao@users.noreply.github.com>
2021-11-22 14:38:57 -0600

    Update AppleandOrange.py

--- AppleandOrange.py
+++ AppleandOrange.py
@@ -5,5 +5,5 @@ def countApplesAndOranges(s, t, a, b, apples, oranges):
         apple_position.append(a + apples[i])
     for i in range(len(oranges)):
         orange_position.append(b+oranges[i])
-    print(len([i for i in apple_position if i>=s and i<=t]))
+    print(len([i for i in apple_position if i>=s and i<=t])) #list comprehension
     print(len([i for i in orange_position if i>=s and i<=t]))
