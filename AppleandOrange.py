52b310cbdf70ca2d28833203ad12f0d443afa07f
catymiao <33770231+catymiao@users.noreply.github.com>
2021-11-22 14:38:31 -0600

    Create AppleandOrange.py

---
+++ AppleandOrange.py
@@ -0,0 +1,9 @@
+def countApplesAndOranges(s, t, a, b, apples, oranges):
+    apple_position = []
+    orange_position = []
+    for i in range(len(apples)):
+        apple_position.append(a + apples[i])
+    for i in range(len(oranges)):
+        orange_position.append(b+oranges[i])
+    print(len([i for i in apple_position if i>=s and i<=t]))
+    print(len([i for i in orange_position if i>=s and i<=t]))
